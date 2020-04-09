0e1dff7872c45a6d4a5bef30cf542c316c3076a0
Rizky Aditya Fahreza <rizkyraf88@gmail.com>
2020-04-10 03:01:35 +0700

    Change Menu

--- application/views/_template/sidebar.php
+++ application/views/_template/sidebar.php
@@ -10,41 +10,29 @@
                 <img src="<?= base_url() ?>assets/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
             </div>
             <div class="pull-left info">
-                <p>Alexander Pierce</p>
+                <p>Rizky Aditya Fahreza</p>
                 <!-- Status -->
                 <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
             </div>
         </div>
 
-        <!-- search form (Optional) -->
-        <form action="#" method="get" class="sidebar-form">
-            <div class="input-group">
-                <input type="text" name="q" class="form-control" placeholder="Search...">
-                <span class="input-group-btn">
-                    <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
-                    </button>
-                </span>
-            </div>
-        </form>
-        <!-- /.search form -->
-
         <!-- Sidebar Menu -->
         <ul class="sidebar-menu" data-widget="tree">
-            <li class="header">HEADER</li>
+            <li class="header">Master Data</li>
             <!-- Optionally, you can add icons to the links -->
-            <li class="active"><a href="#"><i class="fa fa-link"></i> <span>Link</span></a></li>
-            <li><a href="#"><i class="fa fa-link"></i> <span>Another Link</span></a></li>
-            <li class="treeview">
-                <a href="#"><i class="fa fa-link"></i> <span>Multilevel</span>
-                    <span class="pull-right-container">
-                        <i class="fa fa-angle-left pull-right"></i>
-                    </span>
-                </a>
-                <ul class="treeview-menu">
-                    <li><a href="#">Link in level 2</a></li>
-                    <li><a href="#">Link in level 2</a></li>
-                </ul>
-            </li>
+            <li class="active"><a href="#"><i class="fa fa-link"></i> <span>Customer</span></a></li>
+            <li><a href="#"><i class="fa fa-link"></i> <span>Vendor</span></a></li>
+            <li><a href="#"><i class="fa fa-link"></i> <span>Produk</span></a></li>
+            <li><a href="#"><i class="fa fa-link"></i> <span>Kategori Produk</span></a></li>
+            <li class="header">Transaksi</li>
+            <!-- Optionally, you can add icons to the links -->
+            <li><a href="#"><i class="fa fa-link"></i> <span>Transaksi</span></a></li>
+            <li class="header">Laporan</li>
+            <!-- Optionally, you can add icons to the links -->
+            <li><a href="#"><i class="fa fa-link"></i> <span>Transaksi</span></a></li>
+            <li><a href="#"><i class="fa fa-link"></i> <span>Vendor</span></a></li>
+            <li><a href="#"><i class="fa fa-link"></i> <span>Customer</span></a></li>
+            <li><a href="#"><i class="fa fa-link"></i> <span>Barang</span></a></li>
         </ul>
         <!-- /.sidebar-menu -->
     </section>
